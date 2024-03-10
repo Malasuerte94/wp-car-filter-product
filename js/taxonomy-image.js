9f4c9e62cc766b2f808753c33cbf5fcebb675b6b
Malasuerte94 <ene.catalin.94@gmail.com>
2024-03-10 19:55:41 +0200

    Split plugin into multiple files

---
+++ js/taxonomy-image.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function($) {
+  // Media uploader script
+  $(document).on('click', '.ccc_taxonomy_image_button', function(e) {
+    e.preventDefault();
+    let button = $(this);
+    let inputField = $('#ccc_taxonomy_image');
+    let image = wp.media({
+      title: 'Select or Upload Image',
+      button: {
+        text: 'Use Image'
+      },
+      multiple: false
+    });
+
+    image.on('select', function() {
+      var attachment = image.state().get('selection').first().toJSON();
+      inputField.val(attachment.url);
+      button.siblings('.ccc_taxonomy_image_preview').html('<img src="' + attachment.url + '" style="max-width: 100px; max-height: 100px;" />');
+    });
+
+    image.open();
+  });
+});
